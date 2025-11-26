e1ecc08644e3a348a34a4a08182ab3773810348c
chenby <baoyi@nextop.asia>
2025-11-26 15:28:50 +0800

    init

--- Formula/redis-rdb-cli.rb
+++ Formula/redis-rdb-cli.rb
@@ -1,9 +1,9 @@
 class RedisRdbCli < Formula
   desc "CLI tools for parsing, converting Redis RDB files, and migrating Redis data"
   homepage "https://github.com/leonchen83/redis-rdb-cli"
-  url "https://github.com/leonchen83/redis-rdb-cli/releases/download/v0.9.8/redis-rdb-cli-release.tar.gz"
-  version "0.9.8"
-  sha256 "1e17e7379adf6f91679c7e3410e701b7e9dd1f755f69e8e267c02d68645b1105"
+  url "https://github.com/leonchen83/redis-rdb-cli/releases/download/v0.9.9/redis-rdb-cli-release.tar.gz"
+  version "0.9.9"
+  sha256 "2ff5c14a26fc850be4aef270ab3911afab4467e6b33721660dc38096466c024c"
 
   depends_on "openjdk"
 
